a63d9eeadc602489e8bf4091a285849ed9b6b5ab
Yury Bahdanau <yury_bahdanau1@epam.com>
2020-05-05 10:13:34 +0300

    init

---
+++ Module3.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Module3_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+
+    public class Task4
+    {
+        /// <summary>
+        /// Use this method to parse and validate user input
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public bool TryParseNaturalNumber(string input, out int result)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Task5
+    {
+        public int ReverseNumber(int sourceNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Module3.2Tests/Module3.2Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Module3_2.Tests
+{
+    public class Tests4
+    {
+        
+    }
+}
