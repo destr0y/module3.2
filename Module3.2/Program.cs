4c0a7f5bf5eac40b857cc26a72728bc49e1147f3
Yury Bahdanau <yury_bahdanau1@epam.com>
2020-05-05 12:16:06 +0300

    add skeleton

--- Module3.2/Program.cs
+++ Module3.2/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Generic;
 
 namespace Module3_2
 {
@@ -22,11 +22,60 @@ namespace Module3_2
         {
             throw new NotImplementedException();
         }
+
+        public int[] GetFibonacciSequence(int n)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public class Task5
     {
         public int ReverseNumber(int sourceNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Task6
+    {
+        /// <summary>
+        /// Use this method to generate array. It shouldn't throws exception.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public int[] GenerateArray(int size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[] UpdateElementToOppositeSign(int[] source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Task7
+    {
+        /// <summary>
+        /// Use this method to generate array. It shouldn't throws exception.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public int[] GenerateArray(int size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> FindElementGreaterThenPrevious(int[] source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Task8
+    {
+        public int[,] FillArrayInSpiral(int size)
         {
             throw new NotImplementedException();
         }
